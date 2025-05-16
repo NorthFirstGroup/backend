191d0791db67b0e925f689d3840da68ddadebffd
pei8518 <pjwu07@gmail.com>
2025-05-16 19:03:22 +0800

    feat(constants, data-source): add new constants and update data source configuration

---
+++ src/constants/dbEntity.ts
@@ -0,0 +1,12 @@
+
+export const DbEntity = {
+  Area: 'Area',
+  User: 'User',
+  Organizer: 'Organizer',
+  ActivityType: 'ActivityType',
+  Activity: 'Activity',
+  
+  
+  // other tables...
+};
+
