5ac3745deb01218ad8919eb926da37feac7c7d49
Kylo <mylife811203@gmail.com>
2025-05-22 17:22:34 +0800

    feat: 取得單一活動詳細資訊(/api/v1/activity/:activity_id)

--- src/controllers/activity.ts
+++ src/controllers/activity.ts
@@ -2,15 +2,29 @@ import { Response, NextFunction } from 'express'
 import { Request as JWTRequest } from 'express-jwt'
 import getLogger from '../utils/logger'
 import responseSend, { initResponseData } from '../utils/serverResponse'
+import { DbEntity } from '../constants/dbEntity'
+import { dataSource } from '../db/data-source'
 
 const logger = getLogger('Activity')
 
 export async function getActivity(req: JWTRequest, res: Response, next: NextFunction) {
     try {
+    const { activity_id } = req.params
+    // 取得資料
+    const activityRepository = dataSource.getRepository(DbEntity.Activity)
 
-        responseSend(initResponseData(res, 2000))
-    } catch (error) {
-        logger.error('getActivity 錯誤:', error)
-        next(error)
+    const activity = await activityRepository.findOne({
+        where: { id: activity_id },
+        select: ['id', 'name', 'category', 'cover_image', 'description', 'information']
+    })
+
+    if (!activity) {
+      return responseSend(initResponseData(res, 1018))
     }
+
+    return responseSend(initResponseData(res, 2000, activity))
+  } catch (error) {
+    logger.error('getActivity 錯誤', error)
+    next(error)
+  }
 }
